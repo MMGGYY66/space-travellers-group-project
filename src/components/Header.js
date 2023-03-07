b988c34b7c38d8a3078989bf07a1de4883407bb2
gealsanchez <gealsanchez@gmail.com>
2023-03-07 10:31:00 -0500

    Header without styling

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <button type="button"><img src="/planet.png" alt="Logo" width={30} /></button>
+          <h1><Link to="/">Space Travelers Hub</Link></h1>
+          <li><Link to="/">Rockets</Link></li>
+          <li><Link to="/">Missions</Link></li>
+          <li><Link to="/">My Profile</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Rockets.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Rocket = () => (
+  <h1>Rocket List</h1>
+);
+
+export default Rocket;
